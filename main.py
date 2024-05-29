82d74c192798209008d25f1546af0f8484561652
Chrissiez <chrissie.y.zhou@gmail.com>
2024-05-29 16:34:29 -0700

    sigma sorcery

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from api.drink import drink_api
 from api.fitness import fitness_api
 #from api.pulse import pulse_api
 from api.sleep import sleep_api
-from api.titanics import titanic_api
+# from api.titanics import titanic_api
 # database migrations
 from model.users import initUsers
 from model.players import initPlayers
@@ -23,8 +23,8 @@ from model.drinks import initDrinks
 from model.fitnessy import initFitnessy
 #from model.pulses import initPulses 
 from model.sleeps import init_sleep
-from model.titanic import initTitanic
-from model.exercise import initExercise, predictWeight
+# from model.titanic import initTitanic
+# from model.exercise import initExercise, predictWeight
 
 # setup App pages
 from projects.projects import app_projects # Blueprint directory import projects definition
@@ -32,16 +32,16 @@ from projects.projects import app_projects # Blueprint directory import projects
 
 # Initialize the SQLAlchemy object to work with the Flask app instance
 db.init_app(app)
-initTitanic()
+# initTitanic()
 
-initExercise()
+# initExercise()
 # register URIs
 app.register_blueprint(user_api) # register api routes
 app.register_blueprint(player_api)
 app.register_blueprint(app_projects) # register app pages
 app.register_blueprint(drink_api)
 app.register_blueprint(fitness_api)
-app.register_blueprint(titanic_api)
+# app.register_blueprint(titanic_api)
 app.register_blueprint(sleep_api)
 #app.register_blueprint(exercise_api)
 @app.errorhandler(404)  # catch for URL not found
@@ -50,12 +50,12 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 from flask import request, jsonify
-@app.route('/api/predict', methods=['POST'])
-def predict():
-    global exercise_regression
-    contestant = request.get_json()
-    response = predictWeight(contestant)
-    return jsonify(response)
+# @app.route('/api/predict', methods=['POST'])
+# def predict():
+#     global exercise_regression
+#     contestant = request.get_json()
+#     response = predictWeight(contestant)
+#     return jsonify(response)
 # @app.route('/api/predictsurvival', methods=['POST'])
 
 # Define the API endpoint for prediction
@@ -94,4 +94,4 @@ app.cli.add_command(custom_cli)
 # this runs the application on the development server
 if __name__ == "__main__":
     # change name for testing
-    app.run(debug=True, host="0.0.0.0", port="0000")
+    app.run(debug=True, host="0.0.0.0", port="8008")
